963a3660defff5c18e44378559a45fc78ce4109b
ianrob <ianrob@amazon.co.uk>
2025-03-31 15:50:41 +0100

    Update

--- kaled/src/kaled/documents/__init__.py
+++ kaled/src/kaled/documents/__init__.py
@@ -1,3 +1,2 @@
 from .schema import DocBase
-from .kaled_doc import KaledDoc, KaledDocFactory
 from .document_factory import DocumentFactory
---
+++ kaled/src/kaled/documents/kaled/__init__.py
@@ -0,0 +1,1 @@
+from .kaled_doc import KaledDoc, KaledDocFactory
